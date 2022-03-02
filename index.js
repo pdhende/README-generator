8d16cc9a2e9e8605bdd6d7f1c8c9c45e40d79afa
pdhende <pdhendework@gmail.com>
2022-03-01 22:16:33 -0500

    Created questions array of objects

--- index.js
+++ index.js
@@ -4,39 +4,24 @@ const inquirer = require('inquirer');
 const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = ['What is the title of your project?',
-                    'Provide a brief description of your project: ',
-                    'What are the steps required to install your project?',
-                    'Provide instructions for usage: ',
-                    'What license does the project have?',
-                    'Provide any credits: ',
-                    'What is your gitHub Id?',
-                    'What is your email id?',
-                    'What command should be executed to run the tests?'];
+const questions = [{
+            type: "input",
+            name: "title",
+            message: 'What is the title of your project?',
+        },
+        {
+            type: "input",
+            name: "description",
+            message: 'Provide a brief description of your project: ',
+        }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-
     inquirer
-     .prompt([{
-         type: "input",
-         name: "title",
-         message: questions[0],
-     },
-     {
-        type: "input",
-        name: "description",
-        message: questions[1],
-     },
-     {
-        type: "input",
-        name: "installSteps",
-        message: questions[2],
-     },
-     ]);
+     .prompt(questions);
 }
 
 // Function call to initialize app
