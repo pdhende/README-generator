ec7c0f5826c4930103ac17b57965f182c83c3d56
pdhende <pdhendework@gmail.com>
2022-03-02 10:39:24 -0500

    Updated questions array

--- index.js
+++ index.js
@@ -13,7 +13,36 @@ const questions = [{
             type: "input",
             name: "description",
             message: 'Provide a brief description of your project: ',
-        }];
+        },
+        {
+            type: "rawlist",
+            name: "license",
+            message: 'What license does the project have?',
+            choices : ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3','None']
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: 'What command should be run to install dependencies?',
+            default: 'npm i',
+        },
+        {
+            type: "input",
+            name: "githubId",
+            message: 'What is your gitHub username?',
+        },
+        {
+            type: "input",
+            name: "emailId",
+            message: 'What is your email id?',
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: 'What command should be executed to run the tests?',
+            default: 'npm test',
+        }
+    ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
