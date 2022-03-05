1530dbc1d6f38996796e8eef75667e6edcac7bf3
pdhende <pdhendework@gmail.com>
2022-03-05 00:27:31 -0500

    Updated js file to obtain license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let lBadge;
+  // let lBadge;
+  const licObj = {};
   if(license === "MIT") {
-    lBadge = "https://img.shields.io/badge/license-MIT-brightgreen.svg";
+    licObj['lBadge'] = "https://img.shields.io/badge/license-MIT-brightgreen.svg";
+    licObj['lLink'] = "https://choosealicense.com/licenses/mit/";
   }
   else if(license === "Apache 2.0") {
-    lBadge = "https://img.shields.io/badge/license-Apache%202.0-blue.svg";
+    licObj['lBadge'] = "https://img.shields.io/badge/license-Apache%202.0-blue.svg";
+    licObj['lLink'] = "https://choosealicense.com/licenses/apache-2.0/";
   }
   else if(license === "GPL 3.0") {
-    lBadge = "https://img.shields.io/badge/license-GPL%203.0-red.svg";
+    licObj['lBadge'] = "https://img.shields.io/badge/license-GPL%203.0-red.svg";
+    licObj['lLink'] = "https://choosealicense.com/licenses/gpl-3.0/";
   }
   else if(license === "BSL 1") {
-    lBadge = "https://img.shields.io/badge/license-BSL%201-yellow.svg";
+    licObj['lBadge'] = "https://img.shields.io/badge/license-BSL%201-yellow.svg";
+    licObj['lLink'] = "https://choosealicense.com/licenses/bsl-1.0/";
   }
   else {
-    lBadge = '';
+    licObj = {};
   }
-  return lBadge;
+  return licObj;
 }
 
 // TODO: Create a function that returns the license link
@@ -32,13 +37,13 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  let lBadge = renderLicenseBadge(data.license);
+  let licenseObj = renderLicenseBadge(data.license);
   // let lLink = renderLicenseLink(data.license);
   // console.log(lBadge);
   return `
   # ${data.title}
     
-  ![license](${lBadge})
+  ![license](${licenseObj.lBadge})
 
   ## Table of contents
   * [Description](#description)
@@ -82,7 +87,7 @@ function generateMarkdown(data) {
 
   ## License
 
-  This project is licensed under the ${data.license} license.
+  This project is licensed under the [${data.license}](${licenseObj.lLink}) license.
 
   ## Contact
   * Email : ${data.emailId}
